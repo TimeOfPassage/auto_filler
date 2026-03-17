be371e28d746d89db89b2ff3df97a26f907dd187
charles.h <1376246506@qq.com>
2026-03-17 20:10:51 +0800

    去处scripting

--- content.js
+++ content.js
@@ -40,9 +40,6 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
             }
         });
     }
-    //  if (msg.command === 'enableSelection') {
-    //     enableFieldSelection();
-    // }
 });
 
 function setInputValue(el, value) {
@@ -59,36 +56,6 @@ function setInputValue(el, value) {
     el.dispatchEvent(new Event('change', { bubbles: true }));
     el.dispatchEvent(new Event('blur', { bubbles: true }));
 }
-
-
-
-// 启动选择器模式：用户点击选择输入框
-// function enableFieldSelection() {
-//     const listener = e => {
-//         e.preventDefault();
-//         e.stopPropagation();
-//         const selector = getUniqueSelector(e.target);
-//         const value = prompt(`请输入要填充的内容：`, '');
-//         console.log('选择的元素:', selector, '填充的内容:', value);
-//         if (value !== null) {
-//             chrome.runtime.sendMessage({
-//                 command: 'storeField',
-//                 data: { selector, value, url: location.origin }
-//             });
-//         }
-//         document.removeEventListener('click', listener, true);
-//         console.log('选择器模式已关闭');
-//     };
-//     document.addEventListener('click', listener, true);
-// }
-
-// // 简易获取唯一 selector
-// function getUniqueSelector(el) {
-//     if (el.id) return `#${el.id}`;
-//     if (el.name) return `[name="${el.name}"]`;
-//     if (el.className) return `.${el.className.split(' ').join('.')}`;
-//     return el.tagName.toLowerCase();
-// }
 
 // 通用唯一选择器生成器（可增强）
 function getUniqueSelector(el) {
--- popup.js
+++ popup.js
@@ -1,20 +1,3 @@
-// popup.js
-// document.getElementById('fill').addEventListener('click', async () => {
-//     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//     const url = new URL(tab.url).origin;
-//     const config = await getConfig(url);
-//     if (config) {
-//         chrome.tabs.sendMessage(tab.id, { command: 'autofill', data: config });
-//     } else {
-//         document.getElementById('status').innerText = '暂无配置，点击下方配置字段';
-//     }
-// });
-
-// document.getElementById('select').addEventListener('click', async () => {
-//     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-//     chrome.tabs.sendMessage(tab.id, { command: 'enableSelection' });
-// });
-
 // 从 storage 获取配置
 async function getConfig(origin) {
     return new Promise(resolve => {
